174d7fe7a37970e01d92477bde0b1423316206a6
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-02-27 10:31:37 +0200

    Time: 156 ms (88.85%), Space: 48.8 MB (9.11%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+         int j =0;
+        for(int i =0;i<nums.Length;i++)
+        {
+          if(nums[i] != 0)
+          {
+            //Swapping zero with next element until the end of the array
+            int Temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = Temp;
+            j++;
+          }
+        } 
+    }
+}
