22f7d506cb59c9081197df7647962b4dac1f2126
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-03-06 00:04:16 +0200

    Time: 2 ms (7.63%), Space: 43.1 MB (30.23%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode SortedList = null;
+        ListNode Temp = SortedList;
+      while (list1 != null && list2 !=null)
+      {
+          if(list1.val <= list2.val)
+          {
+            if(Temp == null)
+            {
+              Temp = new ListNode(list1.val,null);
+              SortedList = Temp;
+            }else
+            {
+              Temp.next = new ListNode();
+              Temp.next.val = list1.val;
+              Temp = Temp.next;
+            }
+            list1 = list1.next;
+            
+          }else{
+            if(Temp == null)
+            {
+              Temp = new ListNode(list2.val,null);
+              SortedList = Temp;
+            }
+            else
+            {
+              Temp.next = new ListNode();
+              Temp.next.val = list2.val;
+              Temp = Temp.next;
+            }
+            list2 = list2.next;
+          }
+        
+        }
+      while(list1 != null)
+      {
+         if(Temp == null)
+            {
+              Temp = new ListNode(list1.val,null);
+              SortedList = Temp;
+            }else
+            {
+              Temp.next = new ListNode();
+              Temp.next.val = list1.val;
+              Temp = Temp.next;
+            }
+            list1 = list1.next;
+            
+      }
+      while(list2 != null)
+      {
+        if(Temp == null)
+            {
+              Temp = new ListNode(list2.val,null);
+              SortedList = Temp;
+            }
+            else
+            {
+              Temp.next = new ListNode();
+              Temp.next.val = list2.val;
+              Temp = Temp.next;
+            }
+            list2 = list2.next;
+      }
+      return SortedList;
+    }
+}
