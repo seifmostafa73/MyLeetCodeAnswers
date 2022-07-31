9ad990df202b80ea0b7e0c4a3b6657fcff7eb823
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-07-31 15:20:43 +0200

    Time: 277 ms (55.40%), Space: 46.8 MB (66.82%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        Dictionary<int,bool> dict = new Dictionary<int,bool>();
+        
+        for(int i =0;i<nums.Length;i++)
+        {
+            if(dict.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+            dict[nums[i]] = true;
+        }
+        return false;
+    }
+}
