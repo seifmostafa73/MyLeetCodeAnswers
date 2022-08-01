5edbf6c498c9d23447f3c8db12fca3545b74d93f
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-08-01 19:55:11 +0200

    Time: 344 ms (37.91%), Space: 49.4 MB (23.29%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -0,0 +1,46 @@
+public class Solution {
+   //kadne's algorithm O(n)
+    public int MaxSubArray(int[] nums) {
+        if(nums.Length == 1)
+            return nums[0];
+        
+        int sum =  -20000;
+        int maxSum = -20000;
+        for(int i =0;i<nums.Length;i++)
+        {
+            if(nums[i] > sum + nums[i] ) // start a new subarray from this element
+            {
+                sum = nums[i];
+            }
+            else // add the new element to the current sum
+            {
+                sum += nums[i];
+            }
+            maxSum = (sum>maxSum)? sum:maxSum;
+        }
+        return maxSum;
+    }   
+}
+
+//burte force O(n^2)
+/*
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+     int maxSum = int.MinValue;
+     int localSum = 0;
+        
+        for(int start = 0;start < nums.Length;start++)
+        {   localSum = 0;
+            for(int end = start;end < nums.Length;end++)
+            {
+                localSum+=nums[end];
+                if (localSum > maxSum)
+                {
+                    maxSum = localSum;
+                }
+            }
+        }
+        return maxSum;
+    }   
+}
+*/
