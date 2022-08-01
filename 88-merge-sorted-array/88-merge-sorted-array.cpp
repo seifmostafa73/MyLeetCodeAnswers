50ab1687d640f705861c139447998f4c05006ff0
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-08-01 20:44:51 +0200

    Time: 9 ms (12.73%), Space: 9.1 MB (72.00%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,9 +3,10 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(m ==0)
             nums1 = nums2;
+        
       int index = n+m-1;
-        m--;
-        n--;
+      m--;
+      n--;
       while(index > -1 && m>-1 && n>-1)
       {
           if(nums2[n] > nums1[m])
@@ -19,17 +20,19 @@ public:
               m--;
           }
 
-                index --;
+            index --;
         }
+        
         while(index > -1 && m >-1)
         {
-             nums1[index] = nums1[m];
+              nums1[index] = nums1[m];
               m--;
               index --;   
         }
+        
          while(index > -1 && n >-1)
         {
-             nums1[index] = nums2[n];
+              nums1[index] = nums2[n];
               n--;
               index --;
             
