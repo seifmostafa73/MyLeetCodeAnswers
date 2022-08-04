589edd14c0a4819bfd3ab4ae89c35fc3904803f0
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-08-04 19:01:58 +0200

    Time: 38 ms (49.07%), Space: 19.9 MB (42.37%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -4,28 +4,30 @@ public:
         map<char,int> row[9],col[9]; // we have 9 rows and 9 col each should only have an element once 
         map<char,int> subbox[9];    // we have 9 boxes each should only habe an element once
         
-        int  currentbox = 0;
+        int  currentbox = 0; //we will calculate the current box given a row and col 
         
-        for(int R = 0;R < 9;R++)
+        for(int R = 0;R < 9;R++) 
         {   
             for(int C = 0;C < 9;C++)
             {   
-                if(board[R][C] == '.')
+                char currentchar = board[R][C];
+                if(currentchar == '.') // skipping unneccessary chars
                     continue;
                 
                 currentbox = getcurrentbox(R,C);
                 
-                auto it1 = row[R].find(board[R][C]);
-                auto it2 = col[C].find(board[R][C]);
-                auto it3 = subbox[currentbox].find(board[R][C]);
+                auto it1 = row[R].find(currentchar); // checking if we find the current character in the current row
+                auto it2 = col[C].find(currentchar); // ...... col
+                auto it3 = subbox[currentbox].find(currentchar); // .....box
 
                 if(it1 != row[R].end() || it2 != col[C].end() || it3 != subbox[currentbox].end())
                 {
                      return false;                 
-                }
-                row[R].insert(pair<char,int>(board[R][C],true));
-                col[C].insert(pair<char,int>(board[R][C],true));
-                subbox[currentbox].insert(pair<char,int>(board[R][C],true));
+                } 
+                //if we reached here that means we couldn't find the current char , so add it.
+                row[R].insert(pair<char,int>(currentchar,true));
+                col[C].insert(pair<char,int>(currentchar,true));
+                subbox[currentbox].insert(pair<char,int>(currentchar,true));
                 
             }
         }
