27686c43a09d594ad91100bbff7bc291d28c1eac
seifmostafa73 <55613060+seifmostafa73@users.noreply.github.com>
2022-08-09 14:40:20 +0200

    Time: 9 ms (77.60%), Space: 7.2 MB (77.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,31 +5,13 @@ public:
 	{
 		return false;
 	}
-	
-	map<char,int> hashtable1; // stroe freq of charaters of s
-	map<char,int> hashtable2; // stroe freq of charaters of t
-	
-	for(auto c : s)
-	{
-		hashtable1[c] +=1;
+	    vector<int> sv(26,0);
+		vector<int> tv(26,0);
 		
-		//Seeif -> e[2] S,i,f[1]
-	}
-	
-	for(auto c : t)
-	{
-		hashtable2[c] +=1;
-		//Seeif -> e[2] S,i,f[1]
-	}
-	
-	for(int i;i<s.length();i++)
-	{
-		if(hashtable1[s[i]]  != hashtable2[s[i]])
-		{
-			return false;
+		for(int i = 0 ; i < s.size() ; i++){
+			sv[s[i]-'a']++;
+			tv[t[i]-'a']++;
 		}
-	}
-	return true;
-		
+		return sv == tv;
     }
 };
